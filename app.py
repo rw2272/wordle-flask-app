ed7350ecd85f538f6eb06046d340ca530591257c
Raymond <rw2272@gmail.com>
2024-04-13 13:22:58 -0500

    updated for deployment

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 import openai
 from dotenv import dotenv_values
 config = dotenv_values(".env")
@@ -145,8 +146,10 @@ def reset():
     session.clear()
     return redirect('/')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
 
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=False)
 
 
